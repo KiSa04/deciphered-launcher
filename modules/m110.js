c00d5d2c41d8f0b795064196450308f2daf2eaa0
KiSa <55666909+KiSa04@users.noreply.github.com>
2024-02-06 12:40:02 +0000

    Update m110.js

--- modules/m110.js
+++ modules/m110.js
@@ -1,9 +1,9 @@
-var n = { w: "1", e: "2", r: "3", s: "4", d: "5", f: "6", z: "7", x: "8", c: "9", ",": "0", o: "+", a: "*", q: "#" };
+var dialer_numbers = { w: "1", e: "2", r: "3", s: "4", d: "5", f: "6", z: "7", x: "8", c: "9", ",": "0", o: "+", a: "*", q: "#" };
 
-var n110 = {
+var dialer_translator = {
 	translate: function (e) {
-		return n[e] || e;
+		return dialer_numbers[e] || e;
 	},
 };
 
-export default n110;
+export default dialer_translator;
